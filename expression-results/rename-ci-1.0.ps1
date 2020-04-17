05848f1c9a16f0917123e3761548fd2fbac41f10
gravitional <albertains@163.com>
2020-04-17 23:49:59 +0800

    求系数数值的程序，用来查看单项的贡献

---
+++ expression-results/rename-ci-1.0.ps1
@@ -0,0 +1,10 @@
+$tepath="C:\repo-private\paper-2.prd.ci-1.0\"
+
+
+Copy-Item "series``o0``gm``total.ci-1.0.pdf" (Join-Path $tepath "fig2.pdf")
+Copy-Item "series``o1``ge``total.ci-1.0.pdf" (Join-Path $tepath "fig3.pdf")
+Copy-Item "series``o1``gm``total.ci-1.0.pdf" (Join-Path $tepath "fig4.pdf")
+copy-item "fig``baryons``ge``charge.ed3.ci-1.0.pdf" (Join-Path $tepath "fig5.pdf")
+Copy-Item "fig``baryons``ge``neutral.ed3.ci-1.0.pdf" (Join-Path $tepath "fig6.pdf")
+Copy-Item "fig``baryons``gm``charge.ed3.ci-1.0.pdf" (Join-Path $tepath "fig7.pdf")
+Copy-Item "fig``baryons``gm``neutral.ed3.ci-1.0.pdf" (Join-Path $tepath "fig8.pdf")
