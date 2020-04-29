25223e82b0dcafd8e762267b5093d8acdda681fe
gravitional <albertains@163.com>
2020-04-29 16:48:50 +0800

    将 series-o1 series-o0 的 check 文件 重命名以方便查看

--- expression-results/rename-ci-1.5.ps1
+++ expression-results/rename-ci-1.5.ps1
@@ -8,3 +8,8 @@ copy-item "fig``baryons``ge``charge.ed3.ci-1.5.pdf" (Join-Path $tepath "fig5.pdf
 Copy-Item "fig``baryons``ge``neutral.ed3.ci-1.5.pdf" (Join-Path $tepath "fig6.pdf")
 Copy-Item "fig``baryons``gm``charge.ed3.ci-1.5.pdf" (Join-Path $tepath "fig7.pdf")
 Copy-Item "fig``baryons``gm``neutral.ed3.ci-1.5.pdf" (Join-Path $tepath "fig8.pdf")
+
+
+Copy-Item "series``o0``check``gm``total.ci-1.5.pdf" (Join-Path $tepath "fig2a.pdf")
+Copy-Item "series``o1``check``ge``total.ci-1.5.pdf" (Join-Path $tepath "fig3a.pdf")
+Copy-Item "series``o1``check``gm``total.ci-1.5.pdf" (Join-Path $tepath "fig4a.pdf")
