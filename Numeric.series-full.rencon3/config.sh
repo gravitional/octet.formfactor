563bc89802b62895166336e327cfdfd427c9e2a1
gravitional <albertains@163.com>
2021-04-15 00:22:41 +0800

    full 试运行结果

--- Numeric.series-full.rencon3/config.sh
+++ Numeric.series-full.rencon3/config.sh
@@ -2,37 +2,38 @@
 
 order="full"
 prcs='20'
+wlscript='./e.Numeric.series-full.rencon3.strange.wl'
 
-#wolframscript -print "all" -file ./e.Numeric.series-full.rencon3.strange.wl $order '0.90`'${prcs} '1.00`'${prcs}
-#wolframscript -print "all" -file ./e.Numeric.series-full.rencon3.strange.wl $order '0.90`'${prcs} '1.05`'${prcs}
-#wolframscript -print "all" -file ./e.Numeric.series-full.rencon3.strange.wl $order '0.90`'${prcs} '1.10`'${prcs}
-#wolframscript -print "all" -file ./e.Numeric.series-full.rencon3.strange.wl $order '0.90`'${prcs} '1.15`'${prcs}
-#wolframscript -print "all" -file ./e.Numeric.series-full.rencon3.strange.wl $order '0.90`'${prcs} '1.20`'${prcs}
-wolframscript -print "all" -file ./e.Numeric.series-full.rencon3.strange.wl $order '0.90`'${prcs} '1.25`'${prcs}
-wolframscript -print "all" -file ./e.Numeric.series-full.rencon3.strange.wl $order '0.90`'${prcs} '1.30`'${prcs}
-wolframscript -print "all" -file ./e.Numeric.series-full.rencon3.strange.wl $order '0.90`'${prcs} '1.35`'${prcs}
-wolframscript -print "all" -file ./e.Numeric.series-full.rencon3.strange.wl $order '0.90`'${prcs} '1.40`'${prcs}
-wolframscript -print "all" -file ./e.Numeric.series-full.rencon3.strange.wl $order '0.90`'${prcs} '1.45`'${prcs}
-wolframscript -print "all" -file ./e.Numeric.series-full.rencon3.strange.wl $order '0.90`'${prcs} '1.50`'${prcs}
+#wolframscript -print "all" -file $wlscript $order   '0.90`'${prcs}   '1.00`'${prcs}
+#wolframscript -print "all" -file $wlscript $order   '0.90`'${prcs}   '1.05`'${prcs}
+#wolframscript -print "all" -file $wlscript $order   '0.90`'${prcs}   '1.10`'${prcs}
+#wolframscript -print "all" -file $wlscript $order   '0.90`'${prcs}   '1.15`'${prcs}
+#wolframscript -print "all" -file $wlscript $order   '0.90`'${prcs}   '1.20`'${prcs}
+#wolframscript -print "all" -file $wlscript $order   '0.90`'${prcs}   '1.25`'${prcs}
+#wolframscript -print "all" -file $wlscript $order   '0.90`'${prcs}   '1.30`'${prcs}
+#wolframscript -print "all" -file $wlscript $order   '0.90`'${prcs}   '1.35`'${prcs}
+#wolframscript -print "all" -file $wlscript $order   '0.90`'${prcs}   '1.40`'${prcs}
+#wolframscript -print "all" -file $wlscript $order   '0.90`'${prcs}   '1.45`'${prcs}
+#wolframscript -print "all" -file $wlscript $order   '0.90`'${prcs}   '1.50`'${prcs}
 #####################
-#wolframscript -print "all" -file ./e.Numeric.series-full.rencon3.strange.wl $order '0.80`'${prcs} '1.00`'${prcs}
-#wolframscript -print "all" -file ./e.Numeric.series-full.rencon3.strange.wl $order '0.82`'${prcs} '1.00`'${prcs}
-#wolframscript -print "all" -file ./e.Numeric.series-full.rencon3.strange.wl $order '0.84`'${prcs} '1.00`'${prcs}
-#wolframscript -print "all" -file ./e.Numeric.series-full.rencon3.strange.wl $order '0.86`'${prcs} '1.00`'${prcs}
-#wolframscript -print "all" -file ./e.Numeric.series-full.rencon3.strange.wl $order '0.88`'${prcs} '1.00`'${prcs}
-#wolframscript -print "all" -file ./e.Numeric.series-full.rencon3.strange.wl $order '0.92`'${prcs} '1.00`'${prcs}
-#wolframscript -print "all" -file ./e.Numeric.series-full.rencon3.strange.wl $order '0.94`'${prcs} '1.00`'${prcs}
-#wolframscript -print "all" -file ./e.Numeric.series-full.rencon3.strange.wl $order '0.96`'${prcs} '1.00`'${prcs}
-#wolframscript -print "all" -file ./e.Numeric.series-full.rencon3.strange.wl $order '0.98`'${prcs} '1.00`'${prcs}
-#wolframscript -print "all" -file ./e.Numeric.series-full.rencon3.strange.wl $order '1.00`'${prcs} '1.00`'${prcs}
+wolframscript -print "all" -file $wlscript $order   '0.80`'${prcs}   '1.00`'${prcs}
+wolframscript -print "all" -file $wlscript $order   '0.82`'${prcs}   '1.00`'${prcs}
+wolframscript -print "all" -file $wlscript $order   '0.84`'${prcs}   '1.00`'${prcs}
+wolframscript -print "all" -file $wlscript $order   '0.86`'${prcs}   '1.00`'${prcs}
+wolframscript -print "all" -file $wlscript $order   '0.88`'${prcs}   '1.00`'${prcs}
+wolframscript -print "all" -file $wlscript $order   '0.92`'${prcs}   '1.00`'${prcs}
+wolframscript -print "all" -file $wlscript $order   '0.94`'${prcs}   '1.00`'${prcs}
+wolframscript -print "all" -file $wlscript $order   '0.96`'${prcs}   '1.00`'${prcs}
+wolframscript -print "all" -file $wlscript $order   '0.98`'${prcs}   '1.00`'${prcs}
+wolframscript -print "all" -file $wlscript $order   '1.00`'${prcs}   '1.00`'${prcs}
 #####################
-#wolframscript -print "all" -file ./e.Numeric.series-full.rencon3.strange.wl $order '0.80`'${prcs} '1.50`'${prcs}
-#wolframscript -print "all" -file ./e.Numeric.series-full.rencon3.strange.wl $order '0.82`'${prcs} '1.50`'${prcs}
-#wolframscript -print "all" -file ./e.Numeric.series-full.rencon3.strange.wl $order '0.84`'${prcs} '1.50`'${prcs}
-#wolframscript -print "all" -file ./e.Numeric.series-full.rencon3.strange.wl $order '0.86`'${prcs} '1.50`'${prcs}
-#wolframscript -print "all" -file ./e.Numeric.series-full.rencon3.strange.wl $order '0.88`'${prcs} '1.50`'${prcs}
-#wolframscript -print "all" -file ./e.Numeric.series-full.rencon3.strange.wl $order '0.92`'${prcs} '1.50`'${prcs}
-#wolframscript -print "all" -file ./e.Numeric.series-full.rencon3.strange.wl $order '0.94`'${prcs} '1.50`'${prcs}
-#wolframscript -print "all" -file ./e.Numeric.series-full.rencon3.strange.wl $order '0.96`'${prcs} '1.50`'${prcs}
-#wolframscript -print "all" -file ./e.Numeric.series-full.rencon3.strange.wl $order '0.98`'${prcs} '1.50`'${prcs}
-#wolframscript -print "all" -file ./e.Numeric.series-full.rencon3.strange.wl $order '1.00`'${prcs} '1.50`'${prcs}
+#wolframscript -print "all" -file $wlscript $order   '0.80`'${prcs}   '1.50`'${prcs}
+#wolframscript -print "all" -file $wlscript $order   '0.82`'${prcs}   '1.50`'${prcs}
+#wolframscript -print "all" -file $wlscript $order   '0.84`'${prcs}   '1.50`'${prcs}
+#wolframscript -print "all" -file $wlscript $order   '0.86`'${prcs}   '1.50`'${prcs}
+#wolframscript -print "all" -file $wlscript $order   '0.88`'${prcs}   '1.50`'${prcs}
+#wolframscript -print "all" -file $wlscript $order   '0.92`'${prcs}   '1.50`'${prcs}
+#wolframscript -print "all" -file $wlscript $order   '0.94`'${prcs}   '1.50`'${prcs}
+#wolframscript -print "all" -file $wlscript $order   '0.96`'${prcs}   '1.50`'${prcs}
+#wolframscript -print "all" -file $wlscript $order   '0.98`'${prcs}   '1.50`'${prcs}
+#wolframscript -print "all" -file $wlscript $order   '1.00`'${prcs}   '1.50`'${prcs}
