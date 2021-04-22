420db86009f1aeb497aca8a69d9b97c6b3b5c756
gravitional <albertains@163.com>
2021-04-22 16:08:20 +0800

    fix baryons_errobar.sh

--- Numeric.series-full.rencon3/baryons_errobar.sh
+++ Numeric.series-full.rencon3/baryons_errobar.sh
@@ -7,7 +7,7 @@ wlscript='./e.Numeric.series-full.rencon3.strange.wl'
 fitting='Baryons'
 
 ## start the ci=1.0 numeric
-errobarQ = 'L_0.90_ci_1.00'
+errobarQ='L_0.90_ci_1.00'
 declare -a lambdaList=('0.80' '0.90' '1.00')
 
 for lambda in "${lambdaList[@]}"; do
@@ -16,7 +16,7 @@ for lambda in "${lambdaList[@]}"; do
 done
 
 # start the ci=1.5 numeric
-errobarQ = 'L_0.90_ci_1.50'
+errobarQ='L_0.90_ci_1.50'
 declare -a lambdaList=('0.80' '0.90' '1.00')
 for lambda in "${lambdaList[@]}"; do
     wolframscript -print 'all' -file $wlscript $order ${lambda}${prcs} '1.50'${prcs} ${fitting} ${errobarQ}
