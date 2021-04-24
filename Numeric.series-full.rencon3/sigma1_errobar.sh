6923849ebbe6af2f58c7c44692e37c8a4dc88e99
gravitional <albertains@163.com>
2021-04-25 00:17:08 +0800

    sigma1 rela 0.90_1.00,  test run

---
+++ Numeric.series-full.rencon3/sigma1_errobar.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+## 开始主要测试
+order='full'
+prcs='`20'
+wlscript='./e.Numeric.series-full.rencon3.strange.wl'
+fitting='Sigma1'
+
+## start the ci=1.0 numeric
+errobarQ='L_0.90_ci_1.00'
+declare -a lambdaList=('0.80' '1.00')
+
+for lambda in "${lambdaList[@]}"; do
+    wolframscript -print 'all' -file $wlscript $order ${lambda}${prcs} '1.00'${prcs} ${fitting} ${errobarQ}
+    sleep 2
+done
+
+# # start the ci=1.5 numeric
+# errobarQ='L_0.90_ci_1.50'
+# declare -a lambdaList=('0.80' '1.00')
+# for lambda in "${lambdaList[@]}"; do
+#     wolframscript -print 'all' -file $wlscript $order ${lambda}${prcs} '1.50'${prcs} ${fitting} ${errobarQ}
+#     sleep 2
+# done
+
+## 原型
+## wolframscript -print 'all' -file './e.Numeric.series-full.rencon3.strange.wl' 'full' '0.80`20' '1.00`20' 'Sigma1' 'L_0.90_ci_1.00'
