d8c9316ac5b9863e58e8e2ce330cbbb7ffee3288
gravitional <albertains@163.com>
2021-04-25 16:28:53 +0800

    test run sigma1 ci_1.5

--- Numeric.series-full.rencon3/sigma1_errobar.sh
+++ Numeric.series-full.rencon3/sigma1_errobar.sh
@@ -7,21 +7,21 @@ wlscript='./e.Numeric.series-full.rencon3.strange.wl'
 fitting='Sigma1'
 
 ## start the ci=1.0 numeric
-errobarQ='L_0.90_ci_1.00'
-declare -a lambdaList=('0.80' '1.00')
-
-for lambda in "${lambdaList[@]}"; do
-    wolframscript -print 'all' -file $wlscript $order ${lambda}${prcs} '1.00'${prcs} ${fitting} ${errobarQ}
-    sleep 2
-done
-
-# # start the ci=1.5 numeric
-# errobarQ='L_0.90_ci_1.50'
+# errobarQ='L_0.90_ci_1.00'
 # declare -a lambdaList=('0.80' '1.00')
+
 # for lambda in "${lambdaList[@]}"; do
-#     wolframscript -print 'all' -file $wlscript $order ${lambda}${prcs} '1.50'${prcs} ${fitting} ${errobarQ}
+#     wolframscript -print 'all' -file $wlscript $order ${lambda}${prcs} '1.00'${prcs} ${fitting} ${errobarQ}
 #     sleep 2
 # done
+
+# # start the ci=1.5 numeric
+errobarQ='L_0.90_ci_1.50'
+declare -a lambdaList=('0.80' '1.00')
+for lambda in "${lambdaList[@]}"; do
+    wolframscript -print 'all' -file $wlscript $order ${lambda}${prcs} '1.50'${prcs} ${fitting} ${errobarQ}
+    sleep 2
+done
 
 ## 原型
 ## wolframscript -print 'all' -file './e.Numeric.series-full.rencon3.strange.wl' 'full' '0.80`20' '1.00`20' 'Sigma1' 'L_0.90_ci_1.00'
