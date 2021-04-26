e56dab8b9b292e95ee7ad60d706b219ba87511f8
gravitional <albertains@163.com>
2021-04-27 03:11:30 +0800

    add ps1 scripts

---
+++ Numeric.series-o1.rencon3/sigma1_errobar.ps1
@@ -0,0 +1,26 @@
+## powershell script
+
+## 开始主要测试
+$order = 'o1'
+$prcs = '`20'
+$wlscript = '.\e.Numeric.series-o1.rencon3.strange.sea-valence.mass-limit.wl'
+$fitting = 'Sigma1'
+
+## start the ci=1.0 numeric
+$errobarQ = 'L_0.90_ci_1.00'
+${lambdaList} = @('0.80', '1.00')
+foreach (${lambda} in $lambdaList) {
+    wolframscript.exe -print 'all' -file $wlscript $order (${lambda} + ${prcs}) ('1.00' + ${prcs}) ${fitting} ${errobarQ}
+    Start-Sleep -Seconds 2
+}
+
+## start the ci=1.5 numeric
+$errobarQ = 'L_0.90_ci_1.50'
+${lambdaList} = @('0.80', '1.00')
+foreach (${lambda} in $lambdaList) {
+    wolframscript.exe -print 'all' -file $wlscript $order (${lambda} + ${prcs})   ('1.50' + ${prcs})  ${fitting} ${errobarQ}
+    Start-Sleep -Seconds 2
+}
+
+## 原型
+# #wolframscript -print 'all' -file '.\e.Numeric.series-full.rencon3.strange.wl'  'full' ('0.90'+'`20')  ('1.00'+'`20') 'Sigma1'  'L_0.90_ci_1.00'
